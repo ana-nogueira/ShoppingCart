73041da5956c64d32500d50bf4b1c3468b26ef57
Ana Nogueira <acsnogueira95@gmail.com>
2024-04-09 20:49:33 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,6 +1,63 @@
 const cartIcon = document.querySelector(".cartIcon");
+const close = document.querySelector(".close");
 const body = document.querySelector("body");
+const addBtn = document.querySelectorAll(".add-btn");
+const item = document.querySelectorAll(".products .item");
+const listCart = document.querySelector(".listCart-items");
+let arrayDeObjetos = [];
 
 cartIcon.addEventListener("click", () => {
   body.classList.toggle("showCart");
 });
+
+close.addEventListener("click", () => {
+  body.classList.toggle("showCart");
+});
+
+addBtn.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    let parentElement = btn.parentElement;
+    let id = index;
+    let img = parentElement.querySelector(".item-img img").getAttribute("src");
+    let itemName = parentElement.querySelector(".item-name").innerHTML;
+    let itemValue = parentElement
+      .querySelector(".item-value")
+      .innerHTML.slice(2);
+
+    let novoObjeto = new newProduct(index, img, itemName, itemValue);
+    arrayDeObjetos.push(novoObjeto);
+
+    console.log(arrayDeObjetos);
+
+    //addProduct(index, img, itemName, itemValue);
+  });
+});
+
+function addProduct(id, imagem, itemName, itemValue) {
+  let product = document.createElement("div");
+  product.classList.add("item");
+  product.innerHTML = ` <div class="image">
+  <img src="${imagem}" alt="">
+</div>
+<div class="name">
+  ${itemName}
+</div>
+<div class="totalPrice"> $ 
+  ${itemValue}
+</div>
+<div class="quantity">
+  <span class="minus">
+    < </span>
+      <span>0</span>
+      <span class="plus">></span>
+</div>`;
+
+  listCart.appendChild(product);
+}
+
+function newProduct(id, imagem, itemName, itemValue) {
+  this.id = id;
+  this.imagem = imagem;
+  this.itemName = itemName;
+  this.itemValue = itemValue;
+}
